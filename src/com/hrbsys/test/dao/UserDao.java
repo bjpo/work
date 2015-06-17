e5815a22493341147b156d0343b1963d815c113d
hello13uti5 <hello13uti5@sina.com>
2015-06-17 09:36:45 +0800

    清理项目.

--- src/com/hrbsys/test/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.hrbsys.test.dao;
-import java.util.List;
-
-import com.hrbsys.test.bean.FKUser;
-public interface UserDao {
-	public List<FKUser> queryAllUsers();
-	boolean saveUser(FKUser user);
-}
